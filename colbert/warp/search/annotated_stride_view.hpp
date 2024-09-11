5beca97345b1ba44dd9f73a3759e4517559f073f
Luca Scheerer <scheerer@future7.stanford.edu>
2024-09-11 16:06:42 -0700

    merge candidate scores (baseline)

--- colbert/warp/search/annotated_stride_view.hpp
+++ colbert/warp/search/annotated_stride_view.hpp
@@ -12,27 +12,25 @@ struct annotated_stride_view {
 };
 
 std::vector<annotated_stride_view<>> strided_view(
-        const torch::Tensor sizes,
-        torch::Tensor stride_sizes,
-        torch::Tensor pids,
-        torch::Tensor scores) {
-    int32_t *size_ptr = stride_sizes.data_ptr<int32_t>();
+        const torch::Tensor capacities, torch::Tensor sizes,
+        torch::Tensor pids, torch::Tensor scores) {
+    int32_t *size_ptr = sizes.data_ptr<int32_t>();
     int32_t *pids_ptr = pids.data_ptr<int32_t>();
     float *scores_ptr = scores.data_ptr<float>();
 
-    const int num_cells = sizes.size(0);
-    const auto sizes_accessor = sizes.accessor<int64_t, 1>();
+    const int num_cells = capacities.size(0);
+    const auto capacities_accessor = capacities.accessor<int64_t, 1>();
     std::vector<annotated_stride_view<>> views;
     views.reserve(num_cells);
     int64_t begin = 0;
     for (int i = 0; i < num_cells; ++i) {
-        const int64_t size = sizes_accessor[i];
+        const int64_t capacity = capacities_accessor[i];
         views.push_back(annotated_stride_view<>{
             .size_ = size_ptr + i, 
             .keys_ = pids_ptr + begin,
             .data_ = scores_ptr + begin
         });
-        begin += size;
+        begin += capacity;
     }
     return views;
 }
