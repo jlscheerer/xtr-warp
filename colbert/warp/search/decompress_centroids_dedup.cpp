04a1b4aac1d0670f5d40d09dca42ee0afbc8e53f
Luca Scheerer <scheerer@future6.stanford.edu>
2024-09-12 03:09:35 -0700

    faster decompression

--- colbert/warp/search/decompress_centroids_dedup.cpp
+++ colbert/warp/search/decompress_centroids_dedup.cpp
@@ -12,40 +12,54 @@ constexpr uint8_t nbits = NBITS;
 
 template<int8_t nbits>
 float inline __attribute__((always_inline)) decompression_kernel(
-    const torch::TensorAccessor<uint8_t, 1> &residual,
-    const torch::TensorAccessor<float, 2> &bucket_scores) {
+        const uint8_t *residual, const float *bucket_scores) {
     static_assert(nbits == 2 || nbits == 4);
     constexpr int packed_vals_per_byte = 8 / nbits;
     constexpr int packed_dim = dim / packed_vals_per_byte;
+    constexpr uint8_t bucket_dim_shift = nbits;
+
     float score = 0;
     for (int packed_idx = 0; packed_idx < packed_dim; ++packed_idx) {
         const uint8_t packed_val = residual[packed_idx];
         if constexpr (nbits == 2) {
+            // TODO(jlscheerer) Double-check that this code is still correct.
             const uint8_t unpacked_0 = (packed_val & 0xC0) >> 6;
             const uint8_t unpacked_1 = (packed_val & 0x30) >> 4;
             const uint8_t unpacked_2 = (packed_val & 0x0C) >> 2;
             const uint8_t unpacked_3 = (packed_val & 0x03);
 
-            const int unpacked_idx_0 = 4 * packed_idx;
+            // NOTE These correspond to an index into the "dimension"
+            const int unpacked_idx_0 = packed_idx << 2;
             const int unpacked_idx_1 = unpacked_idx_0 + 1;
             const int unpacked_idx_2 = unpacked_idx_0 + 2;
             const int unpacked_idx_3 = unpacked_idx_0 + 3;
 
-            const float score_0 = bucket_scores[unpacked_idx_0][unpacked_0];
-            const float score_1 = bucket_scores[unpacked_idx_1][unpacked_1];
-            const float score_2 = bucket_scores[unpacked_idx_2][unpacked_2];
-            const float score_3 = bucket_scores[unpacked_idx_3][unpacked_3];
+            // NOTE Constrcut the index into the "per dimension" lookup tables
+            const int idx_0 = (unpacked_idx_0 << bucket_dim_shift) | unpacked_0;
+            const int idx_1 = (unpacked_idx_1 << bucket_dim_shift) | unpacked_1;
+            const int idx_2 = (unpacked_idx_2 << bucket_dim_shift) | unpacked_2;
+            const int idx_3 = (unpacked_idx_3 << bucket_dim_shift) | unpacked_3;
+
+            const float score_0 = bucket_scores[idx_0];
+            const float score_1 = bucket_scores[idx_1];
+            const float score_2 = bucket_scores[idx_2];
+            const float score_3 = bucket_scores[idx_3];
 
             score += score_0 + score_1 + score_2 + score_3;
         } else if constexpr (nbits == 4) {
             const uint8_t unpacked_0 = (packed_val & 0xF0) >> 4;
             const uint8_t unpacked_1 = (packed_val & 0x0F);
 
-            const int unpacked_idx_0 = 2 * packed_idx;
+            // NOTE These correspond to an index into the "dimension"
+            const int unpacked_idx_0 = packed_idx << 1;
             const int unpacked_idx_1 = unpacked_idx_0 + 1;
 
-            const float score_0 = bucket_scores[unpacked_idx_0][unpacked_0];
-            const float score_1 = bucket_scores[unpacked_idx_1][unpacked_1];
+            // NOTE Constrcut the index into the "per dimension" lookup tables
+            const int idx_0 = (unpacked_idx_0 << bucket_dim_shift) | unpacked_0;
+            const int idx_1 = (unpacked_idx_1 << bucket_dim_shift) | unpacked_1;
+
+            const float score_0 = bucket_scores[idx_0];
+            const float score_1 = bucket_scores[idx_1];
             score += score_0 + score_1;
         }
     }
@@ -64,6 +78,9 @@ torch_annotated_stride_view<> decompress_centroids_dedup(
     const int nprobe) {
   torch::NoGradGuard no_grad;
   torch::InferenceMode guard;
+  static_assert(nbits == 2 || nbits == 4);
+  constexpr int packed_vals_per_byte = 8 / nbits;
+  constexpr int packed_dim = dim / packed_vals_per_byte;
 
   const int ncells = begins.size(0);
   const auto begins_accessor = begins.accessor<int64_t, 1>();
@@ -76,7 +93,8 @@ torch_annotated_stride_view<> decompress_centroids_dedup(
   const int64_t *sizes_ptr = sizes.data_ptr<int64_t>();
   std::partial_sum(sizes_ptr, sizes_ptr + ncells - 1, offsets.begin() + 1);
 
-  const auto residuals_accessor = residuals_compacted.accessor<uint8_t, 2>();
+  // const auto residuals_accessor = residuals_compacted.accessor<uint8_t, 2>();
+  const uint8_t *residuals_ptr = residuals_compacted.data_ptr<uint8_t>();
   const auto codes_accessor = codes_compacted.accessor<int32_t, 1>();
 
   torch::Tensor stride_sizes = torch::zeros({ncells}, torch::kInt32);
@@ -87,6 +105,7 @@ torch_annotated_stride_view<> decompress_centroids_dedup(
     sizes, stride_sizes, pids, scores
   );
 
+  constexpr uint8_t packed_dim_shift = __builtin_ctz(packed_dim); // log2(packed_dim)
   for (int cell_idx = 0; cell_idx < ncells; ++cell_idx) {
     const int begin = begins_accessor[cell_idx];
     const int n = sizes_accessor[cell_idx];
@@ -95,17 +114,16 @@ torch_annotated_stride_view<> decompress_centroids_dedup(
 
     const auto bucket_scores = torch::matmul(
         Q[cell_idx / nprobe].unsqueeze(1), bucket_weights.unsqueeze(0));
-    const auto bucket_scores_accessor =
-        bucket_scores.accessor<float, 2>();
+    const float *bucket_scores_ptr = bucket_scores.data_ptr<float>();
 
     const auto view = views[cell_idx];
     int32_t pos = -1, prev_pid = -1; float prev_score = 0;
     for (int inner_idx = 0; inner_idx < n; ++inner_idx) {
       const int32_t pid = codes_accessor[begin + inner_idx];
-      const auto &residual = residuals_accessor[begin + inner_idx];
+      const uint8_t *residual = residuals_ptr + ((begin + inner_idx) << packed_dim_shift);
 
       const float score = centroid_score + decompression_kernel<nbits>(
-        residual, bucket_scores_accessor
+        residual, bucket_scores_ptr
       );
       // NOTE directly perform deduplication/max-reduction within the cluster.
       if (prev_pid != pid || score > prev_score) {
